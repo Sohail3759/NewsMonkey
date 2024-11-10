7954d7b322379aa272067797775151a874930f7d
Sohail3759 <164088898+Sohail3759@users.noreply.github.com>
2024-11-10 20:10:05 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Navbar from './Components/Navbar';
+import News from './Components/News';
 
 function App(props) {
   return (
     <div>
-      
+      <Navbar></Navbar>
+      <div className="container"><News pagesize= {5} country = {"us"} category ={"Entertainment"}/></div>
     </div>
   );
 }
